b5e16c684b77e71b09753e0597da75a9431dfb0e
SOJ <olaleyetolu2003@gmail.com>
2025-12-26 21:33:17 +0100

    Fix static files path and css loading

--- URC/settings.py
+++ URC/settings.py
@@ -74,7 +74,7 @@ MIDDLEWARE = [
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'URC.urls'
 
@@ -151,6 +151,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
