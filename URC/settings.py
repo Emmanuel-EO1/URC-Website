0e938f3a2cc55f965129f6e3c14594fd5080727e
SOJ <olaleyetolu2003@gmail.com>
2026-01-17 12:12:49 +0100

    Fix static changes

--- URC/settings.py
+++ URC/settings.py
@@ -65,7 +65,6 @@ if USE_CLOUDINARY:
         'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
         'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
         'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
-        'SECURE': True, # Forces HTTPS for images
     }
 
 CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
