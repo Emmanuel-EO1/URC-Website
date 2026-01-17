91263f53bd3fb35e93359c4dd52cf282e2d7707f
SOJ <olaleyetolu2003@gmail.com>
2026-01-17 09:52:56 +0100

    Dman

--- URC/settings.py
+++ URC/settings.py
@@ -173,12 +173,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 if ENV == "local":
-    STATICFILES_DIRS = [BASE_DIR / "static"]
-
-# Force WhiteNoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+else:
+    # Force WhiteNoise
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary Logic
 USE_CLOUDINARY = os.getenv('USE_CLOUDINARY') == '1'
