2d80f9f8fc4a27260c24be8279ed0a208aef247b
SOJ <olaleyetolu2003@gmail.com>
2026-01-18 11:59:27 +0100

    Force include static source files

--- URC/settings.py
+++ URC/settings.py
@@ -42,8 +42,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'URC', 'static'),
+    BASE_DIR / "static",
     ]
 
 STATICFILES_FINDERS = [
